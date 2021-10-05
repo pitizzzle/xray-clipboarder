d768a79af54c87b38c7d41e45dd5d51c716ea732
pitizzzle <peter.haiduk@fute.dev>
2021-10-05 02:11:20 +0200

    v1.2.0 ui animations

--- popup/main.js
+++ popup/main.js
@@ -1,6 +1,36 @@
-const button = document.querySelector('button');
+const button = document.querySelector('body > button');
+const slogans = document.querySelectorAll('body > div.slogan');
+const rayContainer = document.querySelector('body > div#ray-container');
+
+/* RAY LIFECYCLE:
+    - (1/3) initial/default state outside the viewport
+    - (2/3) hovered state: rays slide into the viewport
+    - hovered rays can either return to initial state (1), or progress to their final state (3)
+    - (3) clicked state: final state outside the viewport, so that rays swipe out perpendicular to their axis
+    - after reaching their final state, rays are disposed
+*/
+
+const getActiveRays = () => document.querySelectorAll('body > div#ray-container > .ray:not(.mode-clicked)');
+
 
 button.addEventListener('click', async () => {
+
+    // create rays, if needed
+    if (!getActiveRays().length) {
+        rayContainer.insertAdjacentHTML('beforeend', `
+            <div class="ray ray-A mode-hovered"></div>
+            <div class="ray ray-B mode-hovered"></div>
+        `)
+    }
+    forceGlobalBrowserReflow();
+
+    // swipe-out rays into their final state and dispose
+    getActiveRays().forEach(ray => {
+        ray.classList.remove('mode-hovered');
+        ray.classList.add('mode-clicked');
+        ray.addEventListener('transitionend', () => { ray.parentNode.removeChild(ray); }, { once: true });
+    })
+
     chrome.tabs.sendMessage(
         (await getCurrentTab()).id,
         { operation: "GET_USER_SELECTION" },
@@ -17,8 +47,8 @@ button.addEventListener('click', async () => {
                     throw chrome.runtime.lastError;
                 }
 
-
                 if (userSelection) {
+                    // actually copy to clipboard xD
                     await navigator.clipboard.writeText(userSelection).catch(err => { });
                     button.className = 'check';
                 }
@@ -34,8 +64,34 @@ button.addEventListener('click', async () => {
     );
 });
 
+
+button.addEventListener('mouseenter', () => {
+    // hide slogans
+    slogans.forEach(slogan => slogan.classList.add('hidden'));
+
+    // create rays, if needed
+    if (!getActiveRays().length) {
+        rayContainer.insertAdjacentHTML('beforeend', `
+            <div class="ray ray-A"></div>
+            <div class="ray ray-B"></div>
+        `)
+    }
+    forceGlobalBrowserReflow();
+
+    // slide-in rays
+    getActiveRays().forEach(ray => ray.classList.add('mode-hovered'));
+});
 button.addEventListener('mouseleave', () => {
+    // reset button icon
     button.className = "";
+
+    // fade-in slogans
+    slogans.forEach(slogan => slogan.classList.remove('hidden'));
+
+    // slide-out rays, if possible/needed
+    if (getActiveRays().length) {
+        getActiveRays().forEach(ray => ray.classList.remove('mode-hovered'));
+    }
 });
 
 
@@ -45,4 +101,14 @@ async function getCurrentTab() {
             resolve(tabs[0]);
         });
     });
+}
+
+function forceGlobalBrowserReflow() {
+    // it does not matter which element's computedStyle is requested, reflow is global in any case
+    const _someElement = document.documentElement;
+
+    // it does not matter which property is accessed (but some property from the `CSSStyleDeclaration` object has to be accessed)
+    const _someProperty = 'width';
+
+    window.getComputedStyle(_someElement).getPropertyValue(_someProperty);
 }
